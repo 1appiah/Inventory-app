e3ec1bdfe1639d8390cc1a8f0c605a03c1cec8ee
1appiah <92669349+1appiah@users.noreply.github.com>
2024-07-25 18:12:00 +0000

    vims

--- inventory/settings.py
+++ inventory/settings.py
@@ -14,6 +14,11 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+from django.core.wsgi import get_wsgi_application
+
+
+application = get_wsgi_application()
+
 
 
 # Quick-start development settings - unsuitable for production
